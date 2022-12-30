5ff1383a28d42b5790fa5891f1e6e6c65264fc3b
Trey <thoopchuk@gmail.com>
2022-12-29 20:00:55 -0600

    fixed a visual bug

--- LearnOpenGL.cpp
+++ LearnOpenGL.cpp
@@ -118,6 +118,8 @@ float lastY = height / 2.0f;
 //frame timing variables
 float deltaTime = 0.0f;	// Time between current frame and last frame
 float lastFrame = 0.0f;
+float currentFrame = 0.0f;
+unsigned int counter = 0;
 
 //window size change input
 void framebuffer_size_callback(GLFWwindow* window, int width, int height){
@@ -197,67 +199,31 @@ int main(){
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
     glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
+
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+    glFrontFace(GL_CCW);
+
     stbi_set_flip_vertically_on_load(true);
 
     Shader asteroidShader("C:/School/C++ Projects/LearnOpenGL/vertexShader.txt", "C:/School/C++ Projects/LearnOpenGL/fragmentShader.txt");
     Shader planetShader("C:/School/C++ Projects/LearnOpenGL/planetVertexShader.txt", "C:/School/C++ Projects/LearnOpenGL/planetFragmentShader.txt");
 
-    Model ourModel("C:/School/C++ Projects/LearnOpenGL/resources/backpack/backpack.obj");
     Model asteroid("C:/School/C++ Projects/LearnOpenGL/resources/block/block.obj");
     Model planet("C:/School/C++ Projects/LearnOpenGL/resources/planet/planet.obj");
 
-    /*glm::mat4 translations[100];
-    int index = 0;
-    for (int y = -10; y < 10; y += 2){
-        for (int x = -10; x < 10; x += 2){
-            glm::mat4 model = glm::mat4(1.0f);
-            model = glm::translate(model, glm::vec3(x, y, 0.0f));
-            translations[index++] = model;
-        }
-    }
-
-    unsigned int instanceVBO;
-    glGenBuffers(1, &instanceVBO);
-    glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(translations), &translations[0], GL_STATIC_DRAW);
-    
-    unsigned int cubeVAO, cubeVBO;
-    glGenVertexArrays(1, &cubeVAO);
-    glGenBuffers(1, &cubeVBO);
-    glBindVertexArray(cubeVAO);
-    glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
-    glEnableVertexAttribArray(3);
-    glVertexAttribPointer(3, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)0);
-    glEnableVertexAttribArray(4);
-    glVertexAttribPointer(4, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)(sizeof(glm::vec4)));
-    glEnableVertexAttribArray(5);
-    glVertexAttribPointer(5, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)(2 * sizeof(glm::vec4)));
-    glEnableVertexAttribArray(6);
-    glVertexAttribPointer(6, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)(3 * sizeof(glm::vec4)));
-
-    glVertexAttribDivisor(3, 1);
-    glVertexAttribDivisor(4, 1);
-    glVertexAttribDivisor(5, 1);
-    glVertexAttribDivisor(6, 1);*/
-
-    unsigned int amount = 100000;
-    glm::mat4* modelMatrices;
+    unsigned int chunkSize = 65536;
+    glm::mat4* chunk;
     int count = 0;
-    modelMatrices = new glm::mat4[amount];
-    srand(static_cast<unsigned int>(glfwGetTime())); // initialize random seed
-    float radius = 150.0;
-    float offset = 25.0f;
+    chunk = new glm::mat4[chunkSize];
     for (int y = 0; y > -256; y--){
         for (int z = -8; z < 8; z++) {
             for (int x = -8; x < 8; x++) {
                 glm::mat4 model = glm::mat4(1.0f);
                 model = glm::translate(model, glm::vec3(x, y, z));
                 model = glm::scale(model, glm::vec3(0.5f));
-                modelMatrices[count++] = model;
+                chunk[count++] = model;
             }
         }
     }
@@ -265,7 +231,7 @@ int main(){
     unsigned int buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, amount * sizeof(glm::mat4), &modelMatrices[0], GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, chunkSize * sizeof(glm::mat4), &chunk[0], GL_STATIC_DRAW);
 
     // set transformation matrices as an instance vertex attribute (with divisor 1)
     // note: we're cheating a little by taking the, now publicly declared, VAO of the model's mesh(es) and adding new vertexAttribPointers
@@ -292,53 +258,20 @@ int main(){
         glBindVertexArray(0);
     }
 
-    ////Generate OpenGL Texture
-    //unsigned int texture;
-    //glGenTextures(1, &texture);
-    //glBindTexture(GL_TEXTURE_2D, texture);
-
-    ////Texture loading
-    //int texWidth, texHeight, nchannels;
-    //unsigned char* data = stbi_load("C:/School/C++ Projects/LearnOpenGL/obamna.png", &texWidth, &texHeight, &nchannels, 0);
-
-    //// set the texture wrapping parameters
-    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    //// set texture filtering parameters
-    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    ////error check and free the memory
-    //if (data){
-    //    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texWidth, texHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-    //    glGenerateMipmap(GL_TEXTURE_2D);
-    //}
-    //else{
-    //    std::cout << "Failed to load texture" << std::endl;
-    //}
-    //stbi_image_free(data);
-
-    ////lets make some buffer objects
-    //unsigned int VBO, VAO, EBO;
-
-    //glGenVertexArrays(1, &VAO);
-    //glGenBuffers(1, &VBO);
-    //glGenBuffers(1, &EBO);
-    //// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-    //glBindVertexArray(VAO);
-
-    //glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    //glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    //glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
     //main game loop, all the rendering and gameplay is happening in here
     while (!glfwWindowShouldClose(window)) {
         //frame timing
-        float currentFrame = glfwGetTime();
+        currentFrame = glfwGetTime();
         deltaTime = currentFrame - lastFrame;
-        lastFrame = currentFrame;
+        counter++;
+        if (deltaTime >= 1.0f / 30.0f) {
+            std::string fps = std::to_string((1.0f / deltaTime) * counter);
+            std::string ms = std::to_string((deltaTime / counter) * 1000);
+            std::string fpsCounter = "LearnOpenGL - " + fps + " FPS / " + ms + " ms";
+            glfwSetWindowTitle(window, fpsCounter.c_str());
+            lastFrame = currentFrame;
+            counter = 0;
+        }
 
         //basic input processing for now
         processInput(window);
@@ -348,7 +281,7 @@ int main(){
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // bind textures on corresponding texture units
-       /* shader.use();
+       /*shader.use();
         shader.setInt("texture1", 0);
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture);*/
@@ -357,51 +290,32 @@ int main(){
         // configure transformation matrices
         glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)width / (float)height, 0.1f, 1000.0f);
         glm::mat4 view = camera.getViewMatrix();
+
         asteroidShader.use();
         asteroidShader.setMat4("projection", projection);
         asteroidShader.setMat4("view", view);
-        planetShader.use();
+
+        /*planetShader.use();
         planetShader.setMat4("projection", projection);
-        planetShader.setMat4("view", view);
+        planetShader.setMat4("view", view);*/
 
         //// draw planet
-        //glm::mat4 model = glm::mat4(1.0f);
-        //model = glm::translate(model, glm::vec3(0.0f, -3.0f, 0.0f));
-        //model = glm::scale(model, glm::vec3(4.0f, 4.0f, 4.0f));
-        //planetShader.setMat4("model", model);
-        //planet.Draw(planetShader);
+        /*glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(0.0f, -3.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(4.0f, 4.0f, 4.0f));
+        planetShader.setMat4("model", model);
+        planet.Draw(planetShader);*/
 
-        // draw meteorites
+        //draw meteorites
         asteroidShader.use();
         asteroidShader.setInt("texture_diffuse1", 0);
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, asteroid.texturesLoaded[0].ID); // note: we also made the textures_loaded vector public (instead of private) from the model class.
-        for (unsigned int i = 0; i < asteroid.meshes.size(); i++)
-        {
+        for (unsigned int i = 0; i < asteroid.meshes.size(); i++) {
             glBindVertexArray(asteroid.meshes[i].VAO);
-            glDrawElementsInstanced(GL_TRIANGLES, static_cast<unsigned int>(asteroid.meshes[i].Indices.size()), GL_UNSIGNED_INT, 0, amount);
+            glDrawElementsInstanced(GL_TRIANGLES, static_cast<unsigned int>(asteroid.meshes[i].Indices.size()), GL_UNSIGNED_INT, 0, chunkSize);
             glBindVertexArray(0);
         }
-
-        //cube stuff
-        // camera/view transformation (mouse movement)
-        //glm::mat4 view = camera.getViewMatrix();
-        //asteroidShader.setMat4("view", view);
-
-        //// pass projection matrix to shader (note that in this case it could change every frame)
-        //glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)width / (float)height, 0.1f, 100.0f);
-        //asteroidShader.setMat4("projection", projection);
-
-        //asteroidShader.use();
-        //glBindVertexArray(cubeVAO);
-        //glDrawArraysInstanced(GL_TRIANGLES, 0, 3, 100); // 100 triangles of 6 vertices each
-        //glBindVertexArray(0);
-
-        //camera/model transformation (rotation, translation)
-        //glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
-        //model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
-        /*model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
-        shader.setMat4("model", model);*/
 
         //swap the front and back buffers and poll for events
         glfwSwapBuffers(window);
